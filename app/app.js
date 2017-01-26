ba2fbfa02f9ffdf7413ddb6cccf9488fad554771
MuhunKim <yale2a11@gmail.com>
2017-01-26 21:47:04 +0900

    Add underscore in page name 'Status_404', 0259eb2d93f71bd66c6918f04dfe96462d9a4795 의견 반영

--- app/app.js
+++ app/app.js
@@ -22,5 +22,5 @@ Object.keys(routes).forEach((route) => {
 	page(route, () => app.ViewComponent = routes[route]);
 });
 
-page('*', () => app.ViewComponent = require('./pages/Status404.vue'));
+page('*', () => app.ViewComponent = require('./pages/Status_404.vue'));
 page();
